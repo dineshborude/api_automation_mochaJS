8e0506d8bc9e428d21adc15f5044119d08636da0
Dinesh Borude <79857147+dineshborude@users.noreply.github.com>
2023-11-08 11:34:20 +0530

    Access Token Generator done
    Now we can generate access token in before call and use it in test.
    Also there is no dependency of other tests to run first, Just import token generator method.

--- config/access_token_generator.js
+++ config/access_token_generator.js
@@ -1,22 +1,28 @@
+import supertest from 'supertest';
+
 const evolvity = supertest('https://staging.humanlytic.com:9000');
 const endpointPath = '/api/v1/superadmin/login';
 
 export const generateAccessToken = async () => {
+  const bodyData = {
+    "username": "hemant.5exceptions@gmail.com",
+    "password": "Admin@1234",
+    "role_id": "3"
+  };
 
-  const bodyData = 
-    {     
-      "username":"hemant.5exceptions@gmail.com",
-      "password":"Admin@1234",
-      "role_id":"3"
-   };
-   const res = await request 
-   evolvity
-   .post(endpointPath)
-   .send(bodyData)
+  try {
+    const res = await evolvity
+      .post(endpointPath)
+      .send(bodyData);
 
-  return res.body.data.login.access_token
+    if (res.status !== 200) {
+      throw new Error(`Failed to authenticate: Status ${res.status}`);
+    }
 
-  
-
-
-}
+    return res.body.data.login.access_token;
+  } catch (error) {
+    // Handle any errors that may occur during the request
+    console.error(error);
+    throw error;
+  }
+};
